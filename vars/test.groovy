e87c2e31b4cbee0dc260327a0f407cc3b5e63fc6
krishna <brkkrishna9@gmail.com>
2023-01-26 15:03:25 +0530

    new approch

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,4 +3,29 @@ def new1() {
     print "hello"
     print "xyz = ${xyz}"
     print "abc is ${abc}"
+
+    if (abc == "Some Data") {
+        print "YES"
+    } else {
+        print "NO"
+    }
+
+    def x = 2
+    def y = 0
+    while(x > y) {
+        println "${y}"
+        y++
+    }
+
+    for(int i = 0;i<5;i++) {
+        println(i);
+    }
+
+    def fruits = ["apple", "banana", "orange"];
+
+    for(i in fruits) {
+        println(i);
+    }
+
+
 }
