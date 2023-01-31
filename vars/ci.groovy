e3a07c2637b08cc05d9883149f471c0523ad48c1
krishna <brkkrishna9@gmail.com>
2023-01-31 20:18:35 +0530

    new approch

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,37 +1,40 @@
-def call()
-{
-    pipeline {
-        agent {
-            label 'workstation'
-        }
-        stages {
-            stage('compile/Build') {
-                steps {
-                    script {
-                        echo 'compile the code'
-                        common.compile()
+def call() {
+    try {
+        pipeline {
+            agent {
+                label 'workstation'
+            }
+            stages {
+                stage('compile/Build') {
+                    steps {
+                        script {
+                            echo 'compile the code'
+                            common.compile()
+                        }
                     }
                 }
-            }
-            stage('unit test cases') {
+                stage('unit test cases') {
                     steps {
                         script {
                             echo 'unit tests'
                             common.unittests()
                         }
                     }
-        }
-        stage('Quality control') {
-            steps {
-                echo 'Quality control'
-            }
-        }
-        stage('upload code to centralized place') {
-            steps {
-                echo 'upload code'
-            }
-        }
+                }
+                stage('Quality control') {
+                    steps {
+                        echo 'Quality control'
+                    }
+                }
+                stage('upload code to centralized place') {
+                    steps {
+                        echo 'upload code'
+                    }
+                }
 
+            }
+        }
+    } catch (Exception e) {
+        common.email("failed")
     }
-}
 }
