be81bb1b0cfe3f9442a4bffe91dbe4636fe7ff60
krishna <brkkrishna9@gmail.com>
2023-02-03 18:11:17 +0530

    new approch

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -23,7 +23,6 @@ def call() {
                 }
                 stage('Quality control') {
                     environment {
-                        echo 'Quality control'
                         SONAR_PASS =  '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                         SONAR_USER =  '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                         //wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
