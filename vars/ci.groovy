ae440f8e509eb6dfb7cc5a42b77bec21b09c8fb0
krishna <brkkrishna9@gmail.com>
2023-02-03 19:07:33 +0530

    new approch

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -23,8 +23,8 @@ def call() {
                 }
                 stage('Quality control') {
                     environment {
-                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_PASS = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\' ' , returnStdout: true).trim()
+                        SONAR_USER = sh (script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\' ', returnStdout: true).trim()
                     }
                     steps {
                         script {
