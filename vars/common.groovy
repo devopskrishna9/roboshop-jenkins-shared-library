1a8525e5260d2241865dd3aabef5674eecbb5ae4
krishna <brkkrishna9@gmail.com>
2023-02-07 03:44:04 +0530

    new changes

--- vars/common.groovy
+++ vars/common.groovy
@@ -40,6 +40,11 @@ def artifactPush() {
         sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
     }
 
+    if (app_lang == "nginx") {
+        sh "zip -r ${component}-${TAG_NAME}.zip *"
+        sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile"
+    }
+
     NEXUS_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     NEXUS_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
