6e130cebffe64fecee8f3a8c558fc5ed0bd5e2a2
krishna <brkkrishna9@gmail.com>
2023-02-11 01:07:04 +0530

    new changes

---
+++ vars/terraform.groovy
@@ -0,0 +1,42 @@
+def call() {
+    pipeline {
+
+        options {
+            ansiColor('xterm')
+        }
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+
+        parameters {
+            string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
+        }
+
+        stages {
+
+            stage('Terraform Init') {
+                steps {
+                    sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+                }
+            }
+
+            stage('Terraform Apply') {
+                steps {
+                    sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+
+    }
+}
