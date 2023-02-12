4523650632469f19377ece1a8c7792e287211f4b
krishna <brkkrishna9@gmail.com>
2023-02-12 18:02:44 +0530

    new changes

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -23,20 +23,7 @@ def call() {
                 }
             }
 
-//            stage('Terraform Apply') {
-//                steps {
-//                    sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
-//                }
-//            }
-
         }
-
-//        post {
-//            always {
-//                cleanWs()
-//            }
-//        }
-
 
     }
 }
