13646b5699d431ac1f731618792b67ebd3efbdc6
krishna <brkkrishna9@gmail.com>
2023-02-12 19:43:18 +0530

    new changes

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -15,7 +15,7 @@ def call() {
 
             stage('Terraform Init') {
                 steps {
-                    sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+                    sh 'terraform init -backend-config=env-${INFRA_ENV}/state.tfvars'
                 }
             }
 
